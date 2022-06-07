02cfca2a0f41fe4e5dd77f330de7ab02136a1403
Raswanth8 <raswanth8901@gmail.com>
2022-06-07 12:33:02 +0530

    Day 3

---
+++ StriverSheet/Day4/twosum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - nums[i]
+            if diff in pMap:
+                return [pMap[diff], i]
+            pMap[n] = i
+        return
